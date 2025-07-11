43917820084f6f395e8a0a08b95022ec0edf6e90
Killua78 <nacimeboubekeur@gmail.com>
2025-07-11 23:11:30 +0200

    header footer bdd

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <title>Mon site e-commerce</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <nav class="navbar navbr-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">Ma boutique</a>
+        </div>
+    </nav>
+    <div class="container my-4">
